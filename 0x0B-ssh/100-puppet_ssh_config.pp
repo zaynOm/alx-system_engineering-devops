b46e7a7b9653c070cb39b1ca102c2dd895efe0fa
zaynOm <ouaziz0626@gmail.com>
2023-12-29 10:37:45 +0100

    task 4: Client configuration file (w/Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,15 @@
 # this manifest makes changes to ssh config file
 
-file { '~/.ssh/config':
+file-line { 'Turn off passwd auth':
+  path    => '~/.ssh/config',
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication',
   ensure  => present,
-  content => @(EOF)
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-  EOF
+}
+
+file-line { 'Declare identity file':
+  path    => '~/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^IdentityFile',
+  ensure  => present,
 }
