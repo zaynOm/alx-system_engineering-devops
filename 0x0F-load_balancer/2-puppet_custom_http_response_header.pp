5ff4238610c112798bffbc5704fca00ae7692fe3
zaynOm <ouaziz0626@gmail.com>
2024-01-09 22:49:46 +0100

    task 2: Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,16 @@
 # This manifest adds a custom HTTP header to nginx server
 
-exec { 'refresh package database':
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
 -> package { 'nginx':
-  ensure => installed,
+  ensure   => installed,
 }
--> file_line { 'add custom header':
-  path  => '/etc/nginx/sites-available/default',
-  after => 'location / {',
-  line  => '\n\tadd_header X-Served-By \"$hostname\";',
+-> file_line { 'add HTTP header':
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "\n\tadd_header X-Served-By \"${hostname}\";",
 }
--> exec { 'restart nginx server':
-  command => '/usr/sbin/service nginx restart',
+-> exec { 'restart nginx':
+    command  => '/usr/sbin/service nginx restart',
 }
