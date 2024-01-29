b398265dc1e84fdd78d367f2fd49736541e0beca
zaynOm <ouaziz0626@gmail.com>
2024-01-29 22:42:41 +0100

    task 0: Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!usr/bin/python3
+"Display Employee tasks"
+import requests
+from sys import argv
+
+
+def fetch():
+    TODOS_URL = "https://jsonplaceholder.typicode.com/todos"
+    USERS_URL = "https://jsonplaceholder.typicode.com/users"
+
+    user_id = argv[1]
+
+    res = requests.get(f"{USERS_URL}/{user_id}")
+    user = res.json()
+
+    res = requests.get(TODOS_URL, params={"userId": user_id})
+    todos = res.json()
+    done_todos = [todo for todo in todos if todo.get("completed") is True]
+
+    print((f"Employee {user.get('name')} is done with tasks("
+           f"{len(done_todos)}/{len(todos)}):"))
+    for item in done_todos:
+        print("\t ", item.get("title"))
+
+
+if __name__ == "__main__":
+    fetch()
