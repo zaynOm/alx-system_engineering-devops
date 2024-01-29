28aa4cbb768d897ff7de23a8ada9172a0d039380
zaynOm <ouaziz0626@gmail.com>
2024-01-29 23:11:06 +0100

    task 1: Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"Display Employee tasks"
+import csv
+import requests
+from sys import argv
+
+
+def fetch():
+    TODOS_URL = "https://jsonplaceholder.typicode.com/todos"
+    USERS_URL = "https://jsonplaceholder.typicode.com/users"
+
+    user_id = argv[1]
+
+    res = requests.get(f"{USERS_URL}/{user_id}")
+    user = res.json()
+
+    res = requests.get(TODOS_URL, params={"userId": user_id})
+    todos = res.json()
+
+    with open(f'{user_id}.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for todo in todos:
+            writer.writerow([user_id, user.get('username'),
+                             todo.get('completed'), todo.get('title')])
+
+
+if __name__ == "__main__":
+    fetch()
