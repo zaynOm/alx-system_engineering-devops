782f43669e0ab16aa34b25e14b42a21f55eba35d
zaynOm <ouaziz0626@gmail.com>
2024-02-14 03:22:05 +0100

    task 0: How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"Get subs count for a subreddit"
+"""Get subs count for a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """Number of subs"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {'User-Agen': 'number_of_subs/1.0'}
-    res = requests.get(url, headers=header)
-    data = res.json()
-
+    res = requests.get(url, headers=header, allow_redirects=False)
     if res.status_code == 200:
+        data = res.json()
         return data.get('data').get('subscribers')
 
     return 0
