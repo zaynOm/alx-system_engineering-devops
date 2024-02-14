18df631584a8f8ba393ff68171ece0ee4db3ec97
zaynOm <ouaziz0626@gmail.com>
2024-02-14 03:38:16 +0100

    task 1: Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Get subs count for a subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    header = {'User-Agen': 'top_ten_posts/1.0'}
+    res = requests.get(url, headers=header, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        posts = data.get('data').get('children')
+        for post in posts:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
